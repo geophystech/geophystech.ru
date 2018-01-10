3db105a56dba02275f59fb8b4b12ccafab2f859d
Vitaliy Klachkov <blackst0ne.ru@gmail.com>
2018-01-11 10:58:11 +1100

    Оптимизирована скорость загрузки страниц (#82)

--- config.rb
+++ config.rb
@@ -65,3 +65,24 @@ end
 
 # Disable warnings
 Haml::TempleEngine.disable_option_validator!
+
+
+module AssetHelper
+  ##
+  # Renders a stylesheet asset inline.
+  def inline_stylesheet(name)
+    content_tag :style do
+      sprockets["#{name}.css"].to_s
+    end
+  end
+
+  ##
+  # Renders a javascript asset inline.
+  def inline_javascript(name)
+    content_tag :script do
+      sprockets["#{name}.js"].to_s
+    end
+  end
+end
+
+helpers AssetHelper
